35854f74605e7412f036a60d2c810a06bf521dbc
danielcsbatista <danielcsbatista@gmail.com>
2026-01-15 20:53:22 -0300

    feat: add config and env file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  return res.json({ chave: "valor" });
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("Select 1 + 1;");
+  console.log(result);
+  return res.status(200).json({ chave: "valor" });
 }
 
 export default status;
