5a9e686aa3eb050d226edf553a1cb25b8cac6a6a
danielcsbatista <danielcsbatista@gmail.com>
2026-03-02 21:24:35 -0300

    feat: Remove console.log on try catch of flow of connections

--- infra/database.js
+++ infra/database.js
@@ -17,14 +17,6 @@ async function query(queryObj) {
   } catch (error) {
     console.error(error);
   } finally {
-    console.log({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      ssl: getSSLValues(),
-    });
     await client.end();
   }
 }
